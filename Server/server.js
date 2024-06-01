d1ac992105c6ac8b5e57bdb197aa98f3c05a65a5
Nantha <nanthu1593@gmail.com>
2024-06-01 23:38:55 +0800

    Server
    Server

---
+++ Server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const ExpressGql =require("express-graphql");
+const mongoose = require("mongoose");
+
+const {
+    GraphQLID
+}  = require("graphql");
+
+
+
+mongoose
