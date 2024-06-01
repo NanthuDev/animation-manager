12f4b4cff8c8e8e030a5850e03026c2a13a05cb5
Nantha <nanthu1593@gmail.com>
2024-06-01 23:38:30 +0800

    Create header.js

---
+++ Client/src/Models/Header/header.js
@@ -0,0 +1,27 @@
+ 
+
+function Header() {
+    return (
+      <div className="App">
+     <header class="bg-white">
+  <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <div class="flex lg:flex-1">
+      <a href="#" class="-m-1.5 p-1.5">
+      <span class="sr-only">Your Company</span> <h1>Animation Management System</h1>
+        </a>
+    </div>
+    <div class="flex lg:hidden"> 
+    </div>
+  
+    <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+     </div>
+  </nav>
+   
+</header>
+
+      </div>
+    );
+  }
+  
+  export default Header;
+  
