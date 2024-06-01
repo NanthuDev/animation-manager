d229ab21fdc8b25b381c34203ab842e35db6845b
Nantha <nanthu1593@gmail.com>
2024-06-01 23:37:54 +0800

    Create Layout.js

---
+++ Client/src/Layout/Layout.js
@@ -0,0 +1,20 @@
+import Header from "../Models/Header/header";
+import NavBar from "../Models/Viewer/NavBar";
+
+ 
+
+function Layout() {
+  return (
+    <div className="App">
+     <Header>
+
+     </Header>
+     <NavBar>
+
+     </NavBar>
+     
+    </div>
+  );
+}
+
+export default Layout;
