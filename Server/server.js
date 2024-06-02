30d25894279c2afa56c2c183df45650ac6cea1ac
Nantha <nanthu1593@gmail.com>
2024-06-03 00:32:18 +0800

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,73 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const ExpressGql =require("express-graphql");
-const mongoose = require("mongoose");
+ const mongoose = require("mongoose");
 
 const {
-    GraphQLID
+    GraphQLID, buildSchema,graphiql
 }  = require("graphql");
 
 
+const app = express();
 
-mongoose
+app.use(bodyParser.json());
+
+const events = [];
+app.use('/graph',ExpressGql.graphqlHTTP({
+    schema:buildSchema(`
+    type Event {
+        _id:ID!,
+        title:String!,
+        Description:String!,
+        price:Float!,
+        date:String!
+
+    }
+
+    input EventInput{
+        title:String!
+        description:String!
+        price:Float!
+        date:String
+    }
+
+    type RootQuery {
+        events:[Event!]!
+    }
+    
+    type RootMutation{
+        createEvent(eventInput:EventInput):Event
+    }
+    
+    schema{
+        query:RootQuery,
+        mutation:RootMutation
+    }`),
+    rootValue:{
+        events:()=>{
+            return events
+        },
+        createEvent:(args)=>{
+            const event = {
+                _id:Math.random().toString(),
+                title:args.title,
+                description:args.description,
+                price:args.price,
+                date:new Date().toISOString()
+
+            }
+            events.push(event)
+            // const eventName = args.name;
+            // return eventName;
+        }
+    },
+    graphiql:true
+}));
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/animation-managers").then(()=>{
+    console.log("Mongo Client Connected")
+}).catch(err=> console.log("Mongo Connection - Error", err))
+
+app.listen(8080)
