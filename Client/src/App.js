6f81ec0498c8d97bb5b82b5a8b9360a8e80e6719
Nantha <nanthu1593@gmail.com>
2024-06-10 02:28:16 +0800

    SW

--- Client/src/App.js
+++ Client/src/App.js
@@ -4,6 +4,25 @@ import Layout from './Layout/Layout';
 
 
 function App() {
+  window.self.addEventListener("fetch", function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open('AMS').then(function(cache) {
+                cache.put(event.request.url, res.clone());
+                return res;
+              });
+            })
+           
+        }
+      })
+    );
+  });
+
   return (
     <div className="App">
     <Layout></Layout>
